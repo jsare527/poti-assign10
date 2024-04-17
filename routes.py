8c81966c4d21ae0b322e76f0a1e5f330e4015739
jsare527 <jsare527@gmail.com>
2024-04-17 14:28:13 -0500

    entries completed

--- routes.py
+++ routes.py
@@ -1,19 +1,30 @@
 from flask import Flask, render_template
+import time
 
 app = Flask(__name__)
 
 
+@app.errorhandler(404)
+def not_found(e):
+    return render_template('404.html')
+
 @app.route('/')
 def home():
-    return render_template('home.html')
+    arr = []
+    with open('entries.txt', 'r') as f:
+        for line in f.readlines():
+            timestamp, title, entry = line.strip().split('|')
+            timestamp = time.ctime((int(timestamp)))
+            arr.append([timestamp, title, entry])
+    return render_template('home.html', data=arr)
 
 @app.route('/about')
 def about():
-    return render_template()
+    return render_template('about.html')
 
 @app.route('/links')
 def links():
-    return render_template()
+    return render_template('links.html')
 
 if __name__ == '__main__':
     app.run()
